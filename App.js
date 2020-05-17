64d6c5c91be54013cf4ddb944c1c6b21cd9e96d6
lioness3 <jcnailtech3@yahoo.com>
2020-05-17 16:41:57 -0700

    retrieve web coordinates

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [lat, setLat]= useState('');
+  const [lng, setLng]= useState('');
+  
+  const  findCoordinates = () => {
+    console.log('here');
+    navigator.geolocation.getCurrentPosition((position) => {
+    
+     
+      
+      setLat(position.coords.latitude)
+      setLng(position.coords.longitude)
+
+    
+      
+    });
+     
+
+
+
+    }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Cant decide where to eat? {lat}</Text>
+    
+      <Button onPress={findCoordinates} title='Share Current Location' />
+      <Button title='Enter a city'/>
     </View>
   );
 }
