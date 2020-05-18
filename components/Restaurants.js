739c2664f76bbdece071014fb07ddc7db3fbe55c
lioness3 <jcnailtech3@yahoo.com>
2020-05-18 03:35:15 -0700

    loading indicator

--- components/Restaurants.js
+++ components/Restaurants.js
@@ -1,6 +1,6 @@
 
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Button,ActivityIndicator } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import * as Location from 'expo-location';
 import Dates from './Dates';
@@ -12,51 +12,77 @@ export default function Restaurants({navigation}) {
   const [lat, setLat]= useState(null);
   const [lng, setLng]= useState(null);
   const [located, setLocated] = useState(false);
-  const [restaurant, setRestaurant] = useState('')
+  const [restaurant, setRestaurant] = useState('');
+  const [randomNumber, setRandomNumber] = useState(0);
+const [loading, setLoading] = useState(true)
 
 
-async function  generateRestaurant() {
- navigator.geolocation.getCurrentPosition((position) => {
-    setLat(position.coords.latitude),
-    setLng(position.coords.longitude),
-    console.log(lat); 
+   const location = navigator.geolocation.getCurrentPosition( (position) =>  {
+      let latitude = position.coords.latitude
+     let longitude =  position.coords.longitude
+    setLat(latitude),
+    setLng(longitude)
+     
+    if(lat & lng){
+      console.log(lat, lng)
+      setLocated(true)
+    }
   })
-  
    
-    let randomNumber = Math.floor(Math.random() * 20);
-   await axios.get(`https://developers.zomato.com/api/v2.1/search?`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'user-key': 'a31bd76da32396a27b6906bf0ca707a2'
-      },
-      params: {
-        'lat':`${lat}`,
-        'lon': `${lng}`,
-
-        // 'lat': '45.5047512',
-        // 'lon': '-122.8462298',
-        'radius':'600',
-        'sort': 'real_distance'
-      }
-    }).then(res => {
-      console.log(res)
-      
-      setRestaurant(res.data.restaurants[randomNumber].restaurant.name)
-    }).catch(err => {
     
-        return setRestaurant('Please share your location so we can better assist you')
-       
-      
-    })
-}
+
+
+  
+
+  
+   useEffect( ()=>{
+    console.log('generating')
+      generateRestaurant()
+  
+   }, [located])
+ 
+
+ async function  generateRestaurant() {
+  setRandomNumber(Math.floor(Math.random() * 20))
+    await axios.get(`https://developers.zomato.com/api/v2.1/search?`, {
+       headers: {
+         'Content-Type': 'application/json',
+         'user-key': 'a31bd76da32396a27b6906bf0ca707a2'
+       },
+       params: {
+         'lat':`${lat}`,
+         'lon': `${lng}`,
+ 
+         // 'lat': '45.5047512',
+         // 'lon': '-122.8462298',
+         'radius':'600',
+         'sort': 'real_distance'
+       }
+     }).then(res => {
+       setRestaurant(res.data.restaurants[randomNumber].restaurant.name),
+       setLoading(false)
+     }).catch(err => {
+       console.log('error',err.message)
+     })
+   
+  }
 
 
 
-  {
+if(loading){
   return(
     <View>
-      <Text>Ive suggested {restaurant} for you! </Text>
-      <Button title='suggest another' onPress={() => generateRestaurant()}/>
+      <ActivityIndicator size='large' color='#95FCF7'/> 
     </View>
-    )}
+    )
+}else{
+  return  (
+    <View>
+    
+    <Text>{restaurant} </Text>
+    <Button title='suggest another' onPress={() => {setLoading(true), generateRestaurant()}}/>
+    
+  </View>
+)
+ }
 }
